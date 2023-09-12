6679ea22ef983b3d38ffd845f01f95356de8f495
VInicius Schuelter <48526428+viniciusschuelter@users.noreply.github.com>
2023-09-12 10:16:28 -0300

    feat(datte): dattebayo api fixed inactivity

--- src/server.ts
+++ src/server.ts
@@ -1,7 +1,9 @@
 import http from 'http';
+import https from 'https';
 import express, { Express } from 'express';
 import routes from './routes/index.route';
 import cors from 'cors';
+import cron from 'node-cron';
 
 const router: Express = express();
 
@@ -18,6 +20,21 @@ router.use((req, res, next) => {
     });
 });
 
+
 const httpServer = http.createServer(router);
 const PORT: any = process.env.PORT ?? 8080;
 httpServer.listen(PORT, () => console.log(`The server is running on port ${PORT}`));
+
+cron.schedule("14 * * * *",
+    () => https.get('https://dattebayo-api.onrender.com/characters/1',
+            resp => getSomeChar(resp))
+                    .on("error", (err) => {console.log("Error: " + err.message);
+    })
+);
+
+
+function getSomeChar(resp: any): any {
+    let data = '';
+    resp.on('data', (chunk: any) => data += chunk);
+    resp.on('end', () =>console.log(JSON.parse(data)));
+}
